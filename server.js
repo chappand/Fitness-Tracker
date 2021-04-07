e9f117c7b8fea1aaf1c5c6b46f96001e99bec568
Andrew Chappell <chappand@gmail.com>
2021-04-06 21:52:32 -0400

    finished html routes, added routes to server.js

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
-// app.use(require("./routes/api.js"));
+app.use(require("./routes/apiroutes.js"));
+app.use(require("./routes/htmlroutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
